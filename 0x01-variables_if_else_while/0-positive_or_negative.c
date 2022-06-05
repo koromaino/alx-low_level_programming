db90c92baf43ca5442f44282dc47b0c12c50f40c
Farah Doe Koroma <koromafarahdoe@gmail.com>
2022-06-05 16:52:34 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,6 @@
 /**
  * main - Prints a random number and states wheither 
  * it is positive, negative, or zero.
- *
  * Return: Always 0.
  */
 int main(void)
@@ -19,7 +18,7 @@ printf("%d is positive\n", n);
 else if (n < 0)
 printf("%d is negative\n", n);
 else
-printf("%d zero\n", n);
+printf("%d is zero\n", n);
 
 return (0);
 }
