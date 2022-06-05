838b9fc97abc6feb954a5e3baea62fbc991f8f71
Farah Doe Koroma <koromafarahdoe@gmail.com>
2022-06-05 15:03:12 -0700

    Number0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,10 +10,10 @@ n = rand() - RAND_MAX / 2;
 if (n > 0 )
 	printf("%d is positive\n", n);
 if (n == 0)
-	printf("%dis zero\n", n);
+	printf("%d is zero\n", n);
 if (n < 0)
 {
-	printf("%d negative\n", n);
+	printf("%d is negative\n", n);
 }
 return(0);
 }
