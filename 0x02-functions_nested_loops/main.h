b9a4358f83bae26b8b80c0653cc654979afd1151
Farah Doe Koroma <koromafarahdoe@gmail.com>
2022-06-08 06:41:26 -0700

    put

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#include "main.h"
+/** main -prints putchar
+ * Return: Always 0
+ */
+int main(void)
+int _putchar(char);{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+	
+}
+
