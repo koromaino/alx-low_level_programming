c55e4eec13365aca03c34956c90d8226d7929ff4
Farah Doe Koroma <koromafarahdoe@gmail.com>
2022-06-21 15:19:15 -0700

    delete to replace

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 #include "main.h"
 
 int _putchar(char c);
-char *_memset(char *s, char b, unsigned int n);
+char *_strchr(char *s, char c);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
